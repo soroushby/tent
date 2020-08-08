2c576f548ae69fbc123024432c47f3ef507433dc
soroushby <49848393+soroushby@users.noreply.github.com>
2020-08-08 10:41:47 -0700

    updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [{ path: '', component: WelcomeComponent }];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
