cd48bf386085921dd8cbd95adaf661aff0a4817a
soroushby <49848393+soroushby@users.noreply.github.com>
2020-08-08 09:33:07 -0700

    creating portfolio component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
+  declarations: [AppComponent, WelcomeComponent, PortfolioComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
